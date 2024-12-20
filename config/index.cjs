310c5f6528ab313f69aa976d22b3fe4217a0241d
DreamHN <129374665+DreamHN@users.noreply.github.com>
2024-12-20 19:05:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,6 +22,7 @@ const USER_CONFIG = {
       name: '亲爱的臭宝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oBNXl6zWpwWjSYw_pD4fM0ku3Tbk',
+      // id: 'oBNXl61qtHOV8NSgkXbYSLkwqCWg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'JiwPB2bdaK34L91H5r1tfVjGgl9rzGeZ2kVXaaH5X_s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
